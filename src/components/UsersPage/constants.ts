f4979fe7f8064cf5e2461907f1dfcd42752211ff
batanoffs <batanoff.s@protonmail.com>
2025-08-08 16:57:19 +0300

    add initial state constant

---
+++ src/components/UsersPage/constants.ts
@@ -0,0 +1,19 @@
+const initialUserValues = {
+  id: undefined,
+  name: '',
+  username: '',
+  email: '',
+  phone: '',
+  website: '',
+  company: {
+    name: '',
+  },
+  address: {
+    street: '',
+    suite: '',
+    city: '',
+    zipcode: '',
+  },
+};
+
+export default initialUserValues;
