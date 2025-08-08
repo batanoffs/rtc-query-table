b09431aa6958231e7d3b9a9a6284432d9d9469be
batanoffs <batanoff.s@protonmail.com>
2025-08-08 16:58:35 +0300

    fix import and tabulation

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,6 +1,6 @@
 import restApi from '../api/api';
 import { combineReducers } from '@reduxjs/toolkit';
-import usersSlice from './slices/userSSlicer';
+import usersSlice from './slices/usersSlicer';
 
 const rootReducer = combineReducers({
   [restApi.reducerPath]: restApi.reducer,
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,17 +1,17 @@
 export type IUser = {
-    id: number;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  address?: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  website?: string;
+  company?: {
     name: string;
-    username: string;
-    email: string;
-    phone: string;
-    address?: {
-        street: string;
-        suite: string;
-        city: string;
-        zipcode: string;
-    };
-    website?: string;
-    company?: {
-        name: string;
-    };
+  };
 };
