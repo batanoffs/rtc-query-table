22adfef8938c4f0d8b85f61f61d910cc8ede6c63
batanoffs <batanoff.s@protonmail.com>
2025-08-11 18:46:41 +0300

    refactor: replace with layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { Layout } from 'antd';
-import UsersPage from './components/UsersPage/UsersPage';
+import { MainLayout } from './layout/MainLayout';
+import { Footer } from './layout/Footer';
+import UsersPage from './pages/UsersPage/UsersPage';
 
 const App = () => {
   return (
-    <Layout style={{ minHeight: '100dvh', backgroundColor: '#f0f2f5', maxHeight: '100dvh' }}>
-      <Layout.Header style={{ color: 'white', textAlign: 'center' }}>
-        <h1>Users Management</h1>
-      </Layout.Header>
-      <Layout.Content style={{ paddingTop: '20px', height: '100%' }}>
-        <div style={{ background: '#fff' }}>
-          <UsersPage />
-        </div>
-      </Layout.Content>
-      <Layout.Footer style={{ textAlign: 'center' }}>
-        © 2025 Created by{' '}
-        <a style={{ color: 'blue', textDecoration: 'underline' }} href="https://daniel-batanov.onrender.com/">
-          batanoffs
-        </a>
-      </Layout.Footer>
-    </Layout>
+    <MainLayout appTitle="User Management App" footerComponent={<Footer />}>
+      <UsersPage />
+    </MainLayout>
   );
 };
 
