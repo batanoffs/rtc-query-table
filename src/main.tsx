0e8ef72c1bc2470630449168ac0106e0211ee251
batanoffs <batanoff.s@protonmail.com>
2025-08-12 10:33:05 +0300

    fix React 19 incompatibility issues, add package provided from AntDesign added ConfigProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import '@ant-design/v5-patch-for-react-19';
+import { ConfigProvider } from 'antd';
 
 import App from './App';
 import store from './store/store';
 
 import './index.css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ConfigProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ConfigProvider>
   </StrictMode>,
 );
