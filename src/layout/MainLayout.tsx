8e554270b8c3873f79d6646dcf1ac370338c8406
batanoffs <batanoff.s@protonmail.com>
2025-08-12 17:26:13 +0300

    update layout styles

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,13 +6,19 @@ type LayoutProps = {
   footerComponent: React.ReactNode;
 };
 
-export const MainLayout: React.FC<LayoutProps> = ({ appTitle, children, footerComponent }) => {
+export const MainLayout: React.FC<LayoutProps> = ({ appTitle, children, footerComponent }: LayoutProps) => {
   return (
     <Layout style={{ minHeight: '100dvh', backgroundColor: '#f0f2f5', maxHeight: '100dvh' }}>
-      <Layout.Header style={{ color: 'white', textAlign: 'center' }}>
+      <Layout.Header
+        style={{
+          color: 'white',
+          textAlign: 'center',
+          paddingTop: '1em',
+        }}
+      >
         <h1>{appTitle}</h1>
       </Layout.Header>
-      <Layout.Content style={{ paddingTop: '20px', height: '100%' }}>{children}</Layout.Content>
+      <Layout.Content style={{ paddingTop: '1em' }}>{children}</Layout.Content>
       <Layout.Footer style={{ textAlign: 'center' }}>{footerComponent}</Layout.Footer>
     </Layout>
   );
