80886302d2986f26e538656a35162e59d585353f
batanoffs <batanoff.s@protonmail.com>
2025-08-12 17:17:59 +0300

    fix short custom import paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: /^~(.+)/,
+        replacement: path.join(process.cwd(), 'node_modules/$1'),
+      },
+      {
+        find: 'assets',
+        replacement: path.join(process.cwd(), 'src/assets'),
+      },
+    ],
+  },
   server: {
     port: 3000,
   },
