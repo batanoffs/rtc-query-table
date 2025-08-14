f84b8e9f5b68b11a04d6e2d26743f421b26831c3
batanoffs <batanoff.s@protonmail.com>
2025-08-14 16:25:34 +0300

    fix: add the theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { ConfigProvider, theme } from 'antd';
 import '@ant-design/v5-patch-for-react-19';
 
 import App from './App';
@@ -11,18 +10,11 @@ import 'antd/dist/reset.css';
 import './index.css';
 import ModalProvider from './theme/providers/Modal/ModalProvider';
 import { NotificationProvider } from './theme/providers/Notification/NotificationProvider';
+import { ThemeProvider } from './theme/providers/ThemeProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConfigProvider
-      theme={{
-        // 1. Use dark algorithm
-        algorithm: theme.darkAlgorithm,
-
-        // 2. Combine dark algorithm and compact algorithm
-        // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
-      }}
-    >
+    <ThemeProvider>
       <ModalProvider>
         <NotificationProvider>
           <Provider store={store}>
@@ -30,6 +22,6 @@ createRoot(document.getElementById('root')!).render(
           </Provider>
         </NotificationProvider>
       </ModalProvider>
-    </ConfigProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
