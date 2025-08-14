af6248c37999ba4cb25f98e4c414109f063dd923
batanoffs <batanoff.s@protonmail.com>
2025-08-14 17:07:41 +0300

    reorder providers to wrap proper content

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import { MainLayout } from './layout/MainLayout';
 import { Footer } from './layout/Footer';
 import UsersPage from './pages/UsersPage/UsersPage';
 import UserTable from './components/Table/UsersTable';
+import { ThemeProvider } from './theme/providers/ThemeProvider';
+import GlobalErrorHandler from './utils/GlobalErrorHandler';
+import ModalProvider from './theme/providers/Modal/ModalProvider';
+import { NotificationProvider } from './theme/providers/Notification/NotificationProvider';
 
 const App: React.FC = () => {
   return (
-    <MainLayout appTitle="User Management App" footerComponent={<Footer />}>
-      <UsersPage Table={UserTable} />
-    </MainLayout>
+    <GlobalErrorHandler>
+      <ThemeProvider>
+        <NotificationProvider>
+          <ModalProvider>
+            <MainLayout appTitle="User Management App" footerComponent={<Footer />}>
+              <UsersPage Table={UserTable} />
+            </MainLayout>
+          </ModalProvider>
+        </NotificationProvider>
+      </ThemeProvider>
+    </GlobalErrorHandler>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import '@ant-design/v5-patch-for-react-19';
 
-import App from './App';
 import store from './store/store';
+import App from './App';
 
+import '@ant-design/v5-patch-for-react-19';
 import 'antd/dist/reset.css';
+
 import './index.css';
-import ModalProvider from './theme/providers/Modal/ModalProvider';
-import { NotificationProvider } from './theme/providers/Notification/NotificationProvider';
-import { ThemeProvider } from './theme/providers/ThemeProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <ModalProvider>
-        <NotificationProvider>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </NotificationProvider>
-      </ModalProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
