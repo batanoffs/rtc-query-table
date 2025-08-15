229a004655eae02500280b2eb69353d73f4f2d77
batanoffs <batanoff.s@protonmail.com>
2025-08-15 16:35:15 +0300

    fix: switch to display properly; add todo for future ref

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -10,17 +10,21 @@ type LayoutProps = {
 
 export const MainLayout: React.FC<LayoutProps> = ({ appTitle, children, footerComponent }: LayoutProps) => {
   const themeCtx = useContext(ThemeContext);
+
   return (
     <Layout style={{ minHeight: '100dvh', backgroundColor: '#f0f2f5', maxHeight: '100dvh' }}>
       <Layout.Header
         style={{
-          color: 'white',
           textAlign: 'center',
           paddingTop: '1em',
+          content: 'test',
         }}
       >
-        <h1>{appTitle}</h1>
-        <Switch defaultChecked onChange={themeCtx?.toggleTheme} />
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <h1 style={{ color: 'white' }}>{appTitle}</h1>
+          {/* TODO you can add icons for Sun and Moon and change them with the toggle */}
+          <Switch title="Theme" defaultChecked onChange={themeCtx?.toggleTheme} />
+        </div>
       </Layout.Header>
       <Layout.Content style={{ paddingTop: '1em' }}>{children}</Layout.Content>
       <Layout.Footer style={{ textAlign: 'center' }}>{footerComponent}</Layout.Footer>
