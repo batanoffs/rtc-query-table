4f67306ef0bc55b6dfa2b7a349859a1377203a6a
batanoffs <batanoff.s@protonmail.com>
2025-08-19 10:38:23 +0300

    simplify nested components and props in UsersPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import { FC } from 'react';
 import { MainLayout } from './layout/MainLayout';
 import { Footer } from './layout/Footer';
 import UsersPage from './pages/UsersPage/UsersPage';
-import UserTable from './pages/UsersPage/components/table/UsersTable';
 import { ThemeProvider } from './theme/providers/ThemeProvider';
 import GlobalErrorHandler from './components/utils/GlobalErrorHandler';
 import ModalProvider from './theme/providers/Modal/ModalProvider';
 import { NotificationProvider } from './theme/providers/Notification/NotificationProvider';
-import CreateUserModal from './pages/UsersPage/components/modal/create/CreateUser';
 
 const App: FC = () => {
   const navigate = useNavigate();
@@ -20,7 +18,7 @@ const App: FC = () => {
         <NotificationProvider>
           <ModalProvider>
             <MainLayout appTitle="User Management App" footerComponent={<Footer />}>
-              <UsersPage table={UserTable} createButton={CreateUserModal} />
+              <UsersPage />
             </MainLayout>
           </ModalProvider>
         </NotificationProvider>
--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,22 +1,26 @@
 import { Flex } from 'antd';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-type UsersPageProps = {
-  table: React.ComponentType<{ toggleOpenClose: () => void }>;
-  createButton: React.ComponentType<{ toggleOpenClose: () => void; isOpen: boolean }>;
-};
+import UserTable from './components/table/UsersTable';
+import { selectSelectedUserId } from '@/store/slices/usersSlicer';
+import { UserModalForm } from './components/modal/UserModalForm';
 
-const UsersPage: React.FC<UsersPageProps> = ({ table: TableComponent, createButton: CreateUserComponent }) => {
+const UsersPage: React.FC = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
-
-  const toggleOpenClose = () => {
-    setOpen((prev) => !prev);
-  };
+  const selectedUserId = useSelector(selectSelectedUserId);
 
   return (
     <Flex justify="center" align="center">
-      <TableComponent toggleOpenClose={toggleOpenClose} />
-      <CreateUserComponent toggleOpenClose={toggleOpenClose} isOpen={isOpen} />
+      <UserTable setOpen={setOpen} />
+      {/* <CreateUserModal toggleOpenClose={toggleOpenClose} isOpen={isOpen} /> */}
+
+      <UserModalForm
+        userId={selectedUserId ?? undefined}
+        isEditMode={!!selectedUserId}
+        isOpen={isOpen}
+        setOpen={setOpen}
+      />
     </Flex>
   );
 };
